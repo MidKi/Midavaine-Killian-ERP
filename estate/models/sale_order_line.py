716b3d71b7b7b21285d6dd197dbd98bd53360177
Tymak72 <80384099+Tymak72@users.noreply.github.com>
2022-12-21 13:40:35 +0100

    update repo dev

---
+++ estate/models/sale_order_line.py
@@ -0,0 +1,7 @@
+from odoo import api, models, _, fields
+from odoo.exceptions import ValidationError
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    training_date = fields.Char(string="Training Date")
